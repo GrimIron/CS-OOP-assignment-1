f5d7fba0aaedae1c8e971739832ee830a55937f4
GrimIron <60586368+GrimIron@users.noreply.github.com>
2020-02-11 13:51:20 +0000

    Basic begining structure

---
+++ Vote Calc/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Vote_Calc
+{
+    class Country
+    {
+        string name;
+        double pop;
+        bool eu;
+
+        static void generate_countries()
+        {
+            Country Austria = new Country();
+            Austria.name = "Austria";
+            Austria.pop = 9006398;
+            Austria.eu = true;
+
+            Country Belgeium = new Country();
+            Belgeium.name = "Belgeium";
+            Belgeium.pop = 11589623;
+            Belgeium.eu = true;
+
+            Country Bulgaria = new Country();
+            Bulgaria.name = "Bulgaria";
+            Bulgaria.pop = 6948445;
+            Bulgaria.eu = false;
+
+            Country Croatia = new Country();
+            Croatia.name = "Croatia";
+            Croatia.pop = 4105267;
+            Croatia.eu = false;
+
+            Country Cyprus = new Country();
+            Cyprus.name = "Cyprus";
+            Cyprus.pop = 1207359;
+            Cyprus.eu = true;
+
+            Country Czech_Republic = new Country();
+            Czech_Republic.name = "Czech Republic";
+            Czech_Republic.pop = 10708981;
+            Czech_Republic.eu = false;
+
+            Country Denmark = new Country();
+            Denmark.name = "Denmark";
+            Denmark.pop = 5792202;
+            Denmark.eu = false;
+
+            Country Estonia = new Country();
+            Estonia.name = "Estonia";
+            Estonia.pop = 1326535;
+            Estonia.eu = true;
+
+            Country Finland = new Country();
+            Finland.name = "Finland";
+            Finland.pop = 5540720;
+            Finland.eu = true;
+
+            Country France = new Country();
+            France.name = "France";
+            France.pop = 65273511;
+            France.eu = true;
+
+            Country Germany = new Country();
+
+        }
+            
+    }
+
+    class 
+}
+    cla
